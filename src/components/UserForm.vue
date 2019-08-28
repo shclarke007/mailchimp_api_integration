1f532037c9a19fe2980b50b8d76e62152708e51f
Shaneil Clarke <shaneilske@yahoo.com>
2019-08-28 15:26:49 +0100

    removed default components. Added form component

---
+++ src/components/UserForm.vue
@@ -0,0 +1,28 @@
+<template>
+  <section>
+    <form action="POST">
+      <label for="name">Name:</label>
+      <input type="text" id="name" v-model="form_data.name"/>
+      <label for="email">Email:</label>
+      <input type="text" id="email" v-model="form_data.email"/>
+    </form>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'UserForm',
+  data(){
+    return{
+      form_data: {
+        name: '',
+        email: ''
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
